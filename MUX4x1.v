3bf7c141c9ee0efede711b00d37a447e0add1b48
ANKESH SINGH <191995624+ankeshsingh00@users.noreply.github.com>
2025-11-26 07:39:25 +0530

    Add files via upload

---
+++ MUX4x1.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.09.2021 14:22:28
+// Design Name: 
+// Module Name: MUX4x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX4x1(
+    input i0,
+    input i1,
+    input i2,
+    input i3,
+    input s1,
+    input s0,
+    output y
+    );
+    wire p,q;
+    MUX_2x1_gate d0 (i0, i1, s0, p);
+    MUX_2x1_gate d1 (i2, i3, s0, q);
+    MUX_2x1_gate d2 (p, q, s1, y);
+        
+endmodule
