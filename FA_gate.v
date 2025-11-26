101f0c5d11f3bebd65595052062f79b5cd08380b
ANKESH SINGH <191995624+ankeshsingh00@users.noreply.github.com>
2025-11-26 07:36:57 +0530

    Clean up header comments in FA_gate.v
    Removed unused header comments from FA_gate.v

--- FA_gate.v
+++ FA_gate.v
@@ -1,9 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.08.2021 22:10:51
+
 // Design Name: 
 // Module Name: FA_gate
 // Project Name: 
